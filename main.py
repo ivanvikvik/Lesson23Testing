3ccfbe51a6fc0efa3f48712b40f347016cf1b3f3
ivanvikvik <ivanvikvik@gmail.com>
2023-04-11 21:37:15 +0300

    Add script of program

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
 from student import Student
+from manager import Manager
 
 
 def main():
+    manager = Manager()
+    s1 = Student("Alex", 20, 10)
+    s2 = Student("Kate", 18, 9)
+    s3 = Student("Peter", 22, 5)
 
+    ls = [s1, s2, "abc", s3, 5]
 
+    result = manager.calc_avg_mark(ls)
 
-    # init(st1, "Alex", 20, 10)
-    # init(st2, "Kate", 18, 9)
-
-    # print(st1.name)
-    # print(getattr(st1, "name"))
-    # print(st1.__getattribute__("name"))
-
-    # del st1.name
-    # delattr(st1, "mark")
-    # st1.__delattr__("age")
-
-    # st1.name = "Alex"
-    # setattr(st1, "name", "Peter")
-    # st1.__setattr__("name", "Olya")
-
-    # print(vars(st1))
-    # print(st1.__dict__)
-    # print(dir(st1))
-    # print(help(st1))
-
+    print(f"Result is {result}.")
 
 if __name__ == '__main__':
     main()
